39f2b1c5bc5fdaef3124961737d60e7ff683bf8d
Jim Toth <jim@artby.city>
2025-09-15 16:09:44 -0400

    Adds spawn script for hyper-aos

--- scripts/spawn.ts
+++ scripts/spawn.ts
@@ -1,93 +1,82 @@
 import 'dotenv/config'
 import { readFileSync } from 'fs'
-import { join, resolve } from 'path'
-import { JWKInterface } from '@ardrive/turbo-sdk/lib/types/common/jwk'
-import { createDataItemSigner, spawn as AoSpawn } from '@permaweb/aoconnect'
+import { connect, createSigner } from '@permaweb/aoconnect'
+import Arweave from 'arweave'
+
 import { logger } from './util/logger'
-import { sendAosMessage } from './util/send-aos-message'
 
-const messagingUnitAddress = process.env.MESSAGING_UNIT_ADDRESS ||
-  'fcoN_xJeisVsPXA-trzVAuIiqO3ydLQxM-L4XbrQKzY'
-const schedulerUnitAddress = process.env.SCHEDULER_UNIT_ADDRESS ||
-  '_GQ33BkPtZrqxA84vM8Zk-N2aO0toNNu_C-l-rawrBA'
-const aosModuleId = process.env.AOS_MODULE_ID ||
-  'QEgxNlbNwBi10VXu5DbP6XHoRDHcynP_Qbq3lpNC97s'
-
-const CONTRACT_NAME = process.env.CONTRACT_NAME || ''
-if (!CONTRACT_NAME) {
-  throw new Error('CONTRACT_NAME is not set!')
-}
-const LUA_SOURCE_TXID = process.env.LUA_SOURCE_TX_ID || ''
-if (!LUA_SOURCE_TXID) {
-  throw new Error('LUA_SOURCE_TX_ID is not set!')
-}
+const HYPERBEAM_URL = process.env.HYPERBEAM_URL || 'https://forward.computer'
+const SCHEDULER = process.env.SCHEDULER
+  || 'NoZH3pueH0Cih6zjSNu_KRAcmg4ZJV1aGHKi0Pi5_Hc'
+const AUTHORITY = process.env.AUTHORITY
+  || 'QWg43UIcJhkdZq6ourr1VbnkwcP762Lppd569bKWYKY'
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
 if (!PRIVATE_KEY) {
   throw new Error('PRIVATE_KEY is not set!')
 }
-const JWK = JSON.parse(readFileSync(PRIVATE_KEY, 'utf-8'))
+const wallet = JSON.parse(readFileSync(PRIVATE_KEY, 'utf-8'))
 
-const SPAWN_TAGS = JSON.parse(process.env.SPAWN_TAGS || '[]')
-if (!Array.isArray(SPAWN_TAGS)) {
-  throw new Error('SPAWN_TAGS must be a JSON array!')
-}
+const MODULE = process.env.MODULE
+  || 'xVcnPK8MPmcocS6zwq1eLmM2KhfyarP8zzmz3UVi1g4'
+const NAME = process.env.PROCESS_NAME
+const GATEWAY = process.env.GATEWAY || 'https://arweave.net'
 
-export async function spawn(
-  contractName: string,
-  luaSourceTxId: string,
-  arweaveWalletJwk: JWKInterface,
-  spawnTags?: { name: string; value: string }[]
-) {
-  logger.info(`Spawning new AO Process for [${contractName}]`)
-  logger.info(`Using LUA Source TX ID [${luaSourceTxId}]`)
-  logger.info(`Using AO Module ID [${aosModuleId}]`)
-  logger.info(`Using Scheduler Unit Address [${schedulerUnitAddress}]`)
-  logger.info(`Using Messaging Unit Address [${messagingUnitAddress}]`)
-  logger.info(`Using Deployer Public Key [${arweaveWalletJwk.n}]`)
-  const signer = createDataItemSigner(arweaveWalletJwk)
+async function spawn() {
+  const address = await Arweave.init({}).wallets.getAddress(wallet)
+  logger.info(`Spawning new hyper-aos process [${NAME || 'unnamed'}]`)
+  logger.info(`Wallet = [${address}]`)
+  logger.info(`Scheduler = [${SCHEDULER}]`)
+  logger.info(`Authority = [${AUTHORITY}]`)
+  logger.info(`Module = [${MODULE}]`)
+  logger.info(`Gateway = [${GATEWAY}]`)
+  logger.info(`Hyperbeam = [${HYPERBEAM_URL}]`)
 
-  logger.info(`Spawning new AO Process...`)
-  if (spawnTags && spawnTags.length > 0) {
-    logger.info(`With additional spawn tags: ${JSON.stringify(spawnTags)}`)
+  const { request } = connect({
+    MODE: 'mainnet',
+    device: 'process@1.0',
+    signer: createSigner(wallet),
+    GATEWAY_URL: GATEWAY,
+    URL: HYPERBEAM_URL
+  })
+  const spawnParams = {
+    path: '/push',
+    method: 'POST',
+    type: 'Process',
+    device: 'process@1.0',
+    'scheduler-device': 'scheduler@1.0',
+    'push-device': 'push@1.0',
+    'execution-device': 'lua@5.3a',
+    'data-protocol': 'ao',
+    variant: 'ao.N.1',
+    'signing-format': 'ANS-104',
+    'app-name': 'hyper-aos',
+    name: NAME,
+    scheduler: SCHEDULER,
+    authority: `${AUTHORITY},fcoN_xJeisVsPXA-trzVAuIiqO3ydLQxM-L4XbrQKzY`,
+    module: MODULE
   }
-  const processId = await AoSpawn({
-    module: aosModuleId,
-    scheduler: schedulerUnitAddress,
-    signer,
-    tags: [
-      { name: 'App-Name', value: 'Wuzzy' },
-      { name: 'Contract-Name', value: contractName },
-      { name: 'Authority', value: messagingUnitAddress },
-      { name: 'Timestamp', value: Date.now().toString() },
-      {
-        name: 'Source-Code-TX-ID',
-        value: luaSourceTxId
-      },
-      ...(spawnTags || [])
-    ]
-  })
+  const spawnResult = await request(spawnParams) as Awaited<
+    ReturnType<typeof request>
+  > & { process: string }
+  logger.info(`Spawn result: [${spawnResult.status}]`)
+  logger.info(`Spawned process id: [${spawnResult.process}]`)
 
-  logger.info(`Sending EVAL of [${contractName}] to AO Process [${processId}]`)
-  await sendAosMessage({
-    processId,
-    data: readFileSync(join(resolve(), `./dist/${contractName}.lua`), 'utf8'),
-    signer,
-    tags: [
-      { name: 'Action', value: 'Eval' },
-      { name: 'App-Name', value: 'Wuzzy' },
-      {
-        name: 'Source-Code-TX-ID',
-        value: luaSourceTxId
-      }
-    ]
-  })
-
-  logger.info(
-    `Process spawned & EVAL action sent for [${contractName}] at [${processId}]`
-  )
+  const evalParams = {
+    type: 'Message',
+    path: `/${spawnResult.process}/push`,
+    method: 'POST',
+    action: 'Eval',
+    'data-protocol': 'ao',
+    target: spawnResult.process,
+    'signing-format': 'ANS-104',
+    accept: 'application/json',
+    data: 'require(".process")._version'
+  }
+  const evalResult = await request(evalParams)
+  logger.info(`Eval result: [${JSON.stringify(evalResult)}]`)
 }
 
-spawn(CONTRACT_NAME, LUA_SOURCE_TXID, JWK, SPAWN_TAGS).then(() => {
+spawn().then(() => {
   logger.info('Spawn AO Process executed successfully!')
 }).catch(error => {
   logger.error(`Error executing spawn AO Process:`, error)
